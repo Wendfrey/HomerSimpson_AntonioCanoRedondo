1826e7fa374ff7c35272395b2394ec1ff9ad8b92
Antonio <toni.cano.redondo@gmail.com>
2019-03-20 11:12:28 +0100

    Apartado B

--- app/src/main/java/com/antonio/homersimpson_antoniocanoredondo/MainActivity.java
+++ app/src/main/java/com/antonio/homersimpson_antoniocanoredondo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.antonio.homersimpson_antoniocanoredondo;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView simpsonTitle = findViewById(R.id.img_simpsontitle);
+        AnimationDrawable animationDrawable = (AnimationDrawable) simpsonTitle.getDrawable();
+        animationDrawable.start();
     }
 }
